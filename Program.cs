f74452b5436538e5efd2f0fc9a4c3cd03b1958a8
lucas_carol <lc.sales.duarte@gmail.com>
2017-07-02 06:50:56 -0300

    First version
    by [Dara Keon (Ísis)] in [2013-04-14 15:30:46 (-03:00)]

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using System.IO;
+
+namespace FileRenamer
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var path = ConfigurationManager.AppSettings["Path"];
+            var format = ConfigurationManager.AppSettings["Format"];
+
+            foreach (var file in Directory.GetFiles(path))
+            {
+                var info = new FileInfo(file);
+
+                var newName = info.LastWriteTime.ToString(format);
+
+                var newCompleteName = Path.Combine(path, newName + info.Extension);
+
+                File.Move(file, newCompleteName);
+             }
+            
+        }
+    }
+}
