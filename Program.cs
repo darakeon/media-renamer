7fd4728db1c85a543b4713d08848dc0a3201ff66
Dara Keon <lc.sales.duarte@gmail.com>
2019-05-20 17:42:29 -0300

    add origin and destiny to config

--- Program.cs
+++ Program.cs
@@ -5,74 +5,71 @@ using System.Linq;
 
 namespace FileRenamer
 {
-    class Program
-    {
-        public static void Main(string[] args)
-        {
-            var path = ConfigurationManager.AppSettings["Path"];
-            var format = ConfigurationManager.AppSettings["Format"];
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			var origin = ConfigurationManager.AppSettings["Origin"];
+			var destiny = ConfigurationManager.AppSettings["Destiny"];
+			var format = ConfigurationManager.AppSettings["Format"];
 
-            var files = Directory.GetFiles(path)
-	            .OrderBy(n => n).ToList();
+			var files = Directory.GetFiles(origin)
+				.OrderBy(n => n).ToList();
 
-            var newDir = Path.Combine(path, "NAMED");
-            if (!Directory.Exists(newDir))
-	            Directory.CreateDirectory(newDir);
+			foreach (var file in files)
+			{
+				String newName = null;
 
-            foreach (var file in files)
-            {
-                String newName = null;
+				for (var s = 0; newName == null; s++)
+				{
+					newName = getNewName(file, format, s);
 
-                for (var s = 0; newName == null; s++)
-                {
-                    newName = getNewName(file, format, s);
+					if (newName == "") continue;
 
-                    if (newName == "") continue;
+					var newCompleteName = Path.Combine(destiny, newName);
 
-					var newCompleteName = Path.Combine(newDir, newName);
-
-                    if (File.Exists(newCompleteName))
-	                    newName = null;
-                    else
-	                    File.Move(file, newCompleteName);
-                }
+					if (File.Exists(newCompleteName))
+						newName = null;
+					else
+						File.Move(file, newCompleteName);
+				}
 
 				Console.WriteLine($"{files.IndexOf(file)}/{files.Count}");
-            }
-            
-        }
+			}
 
-        private static String getNewName(String file, String format, Int32 sumMilliseconds)
-        {
-            using (var image = new ImageHelper(file, sumMilliseconds))
-            {
-                if (image.IsImage)
-                {
-	                var date = image.DateTaken;
+		}
 
-	                if (date == null)
-		                return "";
+		private static String getNewName(String file, String format, Int32 sumMilliseconds)
+		{
+			using (var image = new ImageHelper(file, sumMilliseconds))
+			{
+				if (image.IsImage)
+				{
+					var date = image.DateTaken;
+
+					if (date == null)
+						return "";
 
 					return date.Value.ToString(format)
-                        + image.Extension;
-                }
-            }
+					       + image.Extension;
+				}
+			}
 
-            using (var video = new VideoHelper(file, sumMilliseconds))
-            {
-                if (video.IsVideo)
-                {
-	                var date = video.DateTaken;
+			using (var video = new VideoHelper(file, sumMilliseconds))
+			{
+				if (video.IsVideo)
+				{
+					var date = video.DateTaken;
 
-	                if (date == null)
-		                return "";
+					if (date == null)
+						return "";
 
-                    return date.Value.ToString(format)
-                        + video.Extension;
-                }
-            }
+					return date.Value.ToString(format)
+					       + video.Extension;
+				}
+			}
 
-            return "";
-        }
-    }
+			return "";
+		}
+	}
 }
